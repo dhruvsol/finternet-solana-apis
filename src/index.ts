a33514f581262d0ff8d3f829916a95016efa8e80
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-05-11 11:45:04 -0400

    fix: solana api

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ import { RedisService } from "./services/redis.service";
 const main = async () => {
 	const services = [RedisService];
 
-	for (const service of services) {
-		await service.init();
-	}
+	// for (const service of services) {
+	// 	await service.init();
+	// }
 	const app = new App([new UserController(), new TokenController()]);
 
 	app.listen();
